725d9b203ff5ebee684628422b8fb6ea321eb201
Mads Brøndum <llomul@gmail.com>
2019-12-02 17:54:10 +0100

    Made some changes in Main regarding GUI.

--- src/View/Main.java
+++ src/View/Main.java
@@ -1,21 +1,40 @@
 package View;
 
 import javafx.application.Application;
+import javafx.event.ActionEvent;
+import javafx.event.EventHandler;
 import javafx.fxml.FXMLLoader;
 import javafx.scene.Parent;
 import javafx.scene.Scene;
 import javafx.stage.Stage;
+import javafx.scene.control.Button;
+import javafx.scene.layout.StackPane;
 
-public class Main extends Application {
+public class Main extends Application implements EventHandler<ActionEvent>{
+    Button button;
 
     @Override
     public void start(Stage primaryStage) throws Exception{
-        Parent root = FXMLLoader.load(getClass().getResource("sample.fxml"));
-        primaryStage.setTitle("Hello World");
-        primaryStage.setScene(new Scene(root, 300, 275));
+        primaryStage.setTitle("Streaming Heaven");
+        button = new Button();
+        button.setText("Magnus er dejlig");
+
+        button.setOnAction(this);
+
+        StackPane layout = new StackPane();
+        layout.getChildren().add(button);
+
+        primaryStage.setScene(new Scene(layout, 800, 750));
         primaryStage.show();
     }
 
+    @Override
+    public void handle(ActionEvent event) {
+        if(event.getSource()==button)
+        {
+            System.exit(1);
+        }
+    }
 
     public static void main(String[] args) {
         launch(args);
