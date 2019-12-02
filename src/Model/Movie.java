5d0a3d190df36400bd2203e1d399879159bfc31c
Magnus Pedersen <magnus4p@gmail.com>
2019-12-02 16:55:28 +0100

    Movie now extends Watchable

--- src/Model/Movie.java
+++ src/Model/Movie.java
@@ -1,5 +1,11 @@
 package Model;
 
-public class Movie {
-    String johnny;
+public class Movie extends Watchable{
+    String length;
+
+    public Movie (String length, String title, double rating, String year){
+        super(title,rating, year);
+        this.length = length;
+    }
+
 }
