3261e2fe7aa9eed27761d60790db5cd4cf712951
Magnus Pedersen <magnus4p@gmail.com>
2019-12-02 16:58:35 +0100

    Series now extends Watchable

--- src/Model/Series.java
+++ src/Model/Series.java
@@ -1,5 +1,9 @@
 package Model;
 
-public class Series {
+public class Series extends  Watchable{
+
+    public Series(String title, double rating, String year){
+        super(title, rating, year);
+    }
 
 }
