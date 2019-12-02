49a020c2617af8cf512994296f53923b1fc45854
Mads Brøndum <llomul@gmail.com>
2019-12-02 16:27:51 +0100

    First Commit.

---
+++ src/Model/User.java
@@ -0,0 +1,9 @@
+package Model;
+
+import java.util.List;
+
+public class User {
+    String name;
+    List<Watchable> saved;
+    List<Watchable> watched;
+}
---
+++ src/Model/Watchable.java
@@ -0,0 +1,12 @@
+package Model;
+
+public class Watchable {
+    String name;
+    double rating;
+
+    public Watchable(String name, double rating)
+    {
+        this.name = name;
+        this.rating = rating;
+    }
+}
