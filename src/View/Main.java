1b26e8882907c07977ac44d0a8db7baa19a946fe
Mads Brøndum <llomul@gmail.com>
2019-12-03 11:55:34 +0100

    Made some GUI in Main.

--- src/View/Main.java
+++ src/View/Main.java
@@ -6,34 +6,27 @@ import javafx.event.EventHandler;
 import javafx.fxml.FXMLLoader;
 import javafx.scene.Parent;
 import javafx.scene.Scene;
+import javafx.scene.layout.HBox;
 import javafx.stage.Stage;
 import javafx.scene.control.Button;
 import javafx.scene.layout.StackPane;
 
-public class Main extends Application implements EventHandler<ActionEvent>{
-    Button button;
+public class Main extends Application {
 
     @Override
-    public void start(Stage primaryStage) throws Exception{
-        primaryStage.setTitle("Streaming Heaven");
-        button = new Button();
-        button.setText("Magnus er dejlig");
+    public void start(Stage window) throws Exception{
+        Button movieButton = new Button("Movies");
+        Button seriesButton = new Button("Series");
+        Button settingsButton = new Button("Settings");
+        HBox layoutMain = new HBox();
+        layoutMain.getChildren().addAll(movieButton,seriesButton,settingsButton);
+        Scene sceneMain = new Scene(layoutMain,1000,650);
+        window.setScene(sceneMain);
+        window.setTitle("Streaming Heaven");
+        window.show();
 
-        button.setOnAction(this);
+        movieButton.setOnAction(e -> System.exit(1));
 
-        StackPane layout = new StackPane();
-        layout.getChildren().add(button);
-
-        primaryStage.setScene(new Scene(layout, 800, 750));
-        primaryStage.show();
-    }
-
-    @Override
-    public void handle(ActionEvent event) {
-        if(event.getSource()==button)
-        {
-            System.exit(1);
-        }
     }
 
     public static void main(String[] args) {
