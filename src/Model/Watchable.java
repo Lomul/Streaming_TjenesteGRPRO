01b9ca01de6449b550c8b5c8786fd57be51e636e
Malou <elmelund@hotmail.dk>
2019-12-03 12:23:35 +0100

    Created test image in watchable

--- src/Model/Watchable.java
+++ src/Model/Watchable.java
@@ -1,5 +1,7 @@
 package Model;
 
+import javax.imageio.ImageIO;
+import java.awt.image.BufferedImage;
 import java.util.List;
 
 public class Watchable {
@@ -7,12 +9,18 @@ public class Watchable {
     double rating;
     String year;
     List<String> genre;
+    BufferedImage img;
 
-    public Watchable(String title, double rating, String year)
+    public Watchable(String title, double rating, String year, String imagePath) throws Exception
                 //laver vi en string af genre også ændre dem i konstruktor
                 //eller laver vi en liste af genre også smider det ind som parameter?
     {
         this.title = title;
         this.rating = rating;
+        img = ImageIO.read(getClass().getResource("C:\\Users\\elmel\\IdeaProjects\\Streaming_TjenesteGRPRO\\test.png"));
+    }
+
+    public BufferedImage getImg() {
+        return img;
     }
 }
