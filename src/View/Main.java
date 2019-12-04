71e38c875b1ba72cdcb85a87dd548c2ae0634de3
Mads Brøndum <llomul@gmail.com>
2019-12-04 14:35:56 +0100

    We can now show Jpgs!

--- src/View/Main.java
+++ src/View/Main.java
@@ -4,13 +4,22 @@ import javafx.application.Application;
 import javafx.fxml.FXMLLoader;
 import javafx.scene.Parent;
 import javafx.scene.Scene;
+import javafx.scene.image.Image;
+import javafx.scene.image.ImageView;
+import javafx.scene.layout.HBox;
 import javafx.stage.Stage;
+
+import java.io.FileInputStream;
 
 public class Main extends Application {
 
     @Override
     public void start(Stage primaryStage) throws Exception{
-        Parent root = FXMLLoader.load(getClass().getResource("sample.fxml"));
+        //Parent root = FXMLLoader.load(getClass().getResource("sample.fxml"));
+        Image image = new Image(new FileInputStream("FilmBilleder/Annie Hall.jpg"));
+        ImageView imageView = new ImageView(image);
+        HBox root = new HBox();
+        root.getChildren().add(imageView);
         primaryStage.setTitle("Hello World");
         primaryStage.setScene(new Scene(root, 300, 275));
         primaryStage.show();
