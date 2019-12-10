14a86f803e9f0904bbd414c1bdc86ad70a227427
Magnus Pedersen <magnus4p@gmail.com>
2019-12-10 21:59:49 +0100

    Series yearArray is used now

--- src/View/Main.java
+++ src/View/Main.java
@@ -54,7 +54,7 @@ public class Main extends Application {
     }
 
         public static void main(String[] args) {
-        launch(args);
+        launch(args); //Den her er kun så jeg kan pushe
     }
 
 }
